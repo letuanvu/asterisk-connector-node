de594f3735175bec7e198b189ed4b1c2e02cd162
DESKTOP-OGLR40H\MSI <thongnguyen.ask@gmail.com>
2017-05-24 14:39:21 +0700

    fixed emitter

--- server/webSocket/emitter.js
+++ server/webSocket/emitter.js
@@ -7,15 +7,6 @@ function emitter(ami, socket, dbconnection) {
     console.log('ready');
     ami.on('eventAny', function (data) {
         socket.emit('info', data);
-        if (data.Event == 'Cdr') {
-            dbconnection.execute('SELECT MAX(crmid) AS latestId FROM `vtiger_crmentity`', function (err, results, fields) {
-                var nextId = results[0].latestId + 1; // results contains rows returned by server
-                dbconnection.execute('INSERT INTO vtiger_crmentity (crmid,smcreatorid,smownerid,modifiedby,setype,description,createdtime,modifiedtime,viewedtime,status,version,presence,deleted,label,source,smgroupid) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
-                [nextId, 1, 1, 0, "PBXManager", "", data.StartTime, data.StartTime, null, null, 0, 1, 0, data.Destination, "CRM", 0], function (err, results, fields) {
-
-                });
-            });
-        }
     });
 
 }
